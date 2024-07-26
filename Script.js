a28362a68fc27ba4cb37975585371886c416f23a
Devant <143995773+Devantpal@users.noreply.github.com>
2024-07-26 14:20:12 +0530

    i add some special feature

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 
 // ========================================
-//  animated counter number
+//        animated counter number
 // ========================================
 
 const counterNum = document.querySelectorAll(".counter-numbers");
@@ -26,3 +26,46 @@ counterNum.forEach((curElem) => {
     };
     updateNumber();
 });
+
+
+
+// ========================================
+//   Creating a responsive navbar component
+// ========================================
+
+const moblie_nav = document.querySelector(".moblie-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+moblie_nav.addEventListener("click", () => {
+    headerElem.classList.toggle("active");
+});
+
+
+// ========================================
+//           Scroll to top button
+// ========================================
+
+const footerElement = document.querySelector(".section-footer");
+
+const ScrollElement = document.createElement("div");
+ScrollElement.classList.add("scrollTop-style");
+
+ScrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`;
+
+footerElem.after(ScrollElement);
+
+
+
+// ========================================
+//   Creating a portfolio tabbed componenet
+// ========================================
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll("p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+
+p_btns.addEventListener("click", (e) => {
+    const p_btn_clicked = e.target;
+    console.log(p_btn_clicked);
+});
+
